94eeda7fbf83423e6770108dbb247f19970a5e91
Vlad <160039489+55kt@users.noreply.github.com>
2024-10-03 22:16:26 +0300

    TabView & Extensions

---
+++ OneOnOne/MainTabView.swift
@@ -0,0 +1,91 @@
+//
+//  MainTabView.swift
+//  OneOnOne
+//
+//  Created by Vlad on 3/10/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    
+    // MARK: - Initializer
+    init() {
+        makeTabBarOpaque()
+    }
+    
+    // MARK: - Body
+    var body: some View {
+        TabView {
+            placeholderItemView("Chats")
+                .tabItem {
+                    Image(systemName: Tab.chats.icon)
+                    Text(Tab.chats.title)
+                }
+            placeholderItemView("Search")
+                .tabItem {
+                    Image(systemName: Tab.search.icon)
+                    Text(Tab.search.title)
+                }
+            placeholderItemView("Settings")
+                .tabItem {
+                    Image(systemName: Tab.settings.icon)
+                    Text(Tab.settings.title)
+                }
+        }
+    }
+    
+    // Функция которая делает TabBar постоянно непрозрачным
+    private func makeTabBarOpaque() {
+        let apperance = UITabBarAppearance()
+        apperance.configureWithOpaqueBackground()
+        UITabBar.appearance().standardAppearance = apperance
+        UITabBar.appearance().scrollEdgeAppearance = apperance
+    }
+}
+
+// MARK: - Extensions
+extension MainTabView {
+    
+    // Временный PlaceHolder для тестинга TabBar
+    private func placeholderItemView(_ title: String) -> some View {
+        ScrollView {
+            VStack {
+                ForEach(0..<120) { _ in
+                    Text(title)
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 120)
+                        .background(.green)
+                }
+            }
+        }
+    }
+    
+    // MARK: - TabView Enum
+    private enum Tab: String {
+        case chats, search, settings
+        
+        // Переменная для оформления Text с заглавной буквы
+        fileprivate var title: String {
+            return rawValue.capitalized
+        }
+        
+        fileprivate var icon: String {
+            switch self {
+            case .chats:
+                return "bubble.left"
+            case .search:
+                return "magnifyingglass"
+            case .settings:
+                return "gear"
+            }
+        }
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    MainTabView()
+}
--- OneOnOne/OneOnOneApp.swift
+++ OneOnOne/OneOnOneApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OneOnOneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
