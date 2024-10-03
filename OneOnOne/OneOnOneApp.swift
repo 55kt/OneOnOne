fc8b4a5cac3d381716ce5166ced5a36b6a575f59
Vlad <160039489+55kt@users.noreply.github.com>
2024-10-03 21:25:18 +0300

    First commit. "Empty App"

---
+++ OneOnOne/OneOnOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneOnOneApp.swift
+//  OneOnOne
+//
+//  Created by Vlad on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OneOnOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
