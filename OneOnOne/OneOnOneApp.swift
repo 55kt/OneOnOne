cc38a031caa352dfcb08412a078d504f859c39c4
Vlad <160039489+55kt@users.noreply.github.com>
2024-10-20 17:40:54 +0300

    discard changes

--- OneOnOne/OneOnOneApp.swift
+++ OneOnOne/OneOnOneApp.swift
@@ -9,21 +9,13 @@ import SwiftUI
 import Firebase
 import FirebaseAuth
 
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         
         return true
     }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Auth.auth().setAPNSToken(deviceToken, type: .prod)
-    }
-    
-//    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) async -> UIBackgroundFetchResult {
-//        return .noData
-//    }
 }
 
 @main
